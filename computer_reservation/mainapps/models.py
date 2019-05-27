75cf59f4d2e097bce22ec2d05d44132872001fd8
prapawity <60070045@kmitl.ac.th>
2019-05-27 22:53:03 +0700

    prototype 1

--- computer_reservation/mainapps/models.py
+++ computer_reservation/mainapps/models.py
@@ -2,5 +2,16 @@ from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+class Computer(models.Model):
+    status = models.BooleanField(default=True)
 class Student(models.Model):
-    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    surname = models.CharField(max_length=255)
+    year = models.IntegerField()
+class Reservation(models.Model):
+    student = models.ForeignKey(Student,on_delete=models.CASCADE)
+    computer = models.ForeignKey(Computer,on_delete=models.CASCADE)
+    date = models.CharField(max_length=255)
+    start_date = models.CharField(max_length=255)
+    end_date = models.CharField(max_length=255)
+
--- computer_reservation/mainapps/views.py
+++ computer_reservation/mainapps/views.py
@@ -1,8 +1,43 @@
 from django.shortcuts import render
 
 # Create your views here.
+from mainapps.models import Computer, Reservation, Student
+
 
 def index(request):
+    if request.method == 'POST':
+        if (request.POST.get('student_id') == request.POST.get('Password')):
+            if (request.POST.get('student_name') == None):
+                data = Reservation.objects.get(computer_id=int(request.POST.get('id_com')))
+                data.computer.status = True
+                data.computer.save()
+                data.delete()
+            else:
+                chk = False
+                for i in Student.objects.all():
+                    if (i.id == int(request.POST.get('student_id'))):
+                        chk = True
+                if (chk == False):
+                    Student.objects.create(
+                        id=int(request.POST.get('student_id')),
+                        name=request.POST.get('student_name'),
+                        surname=request.POST.get('student_surname'),
+                        year=int(request.POST.get('student_year')),
+                    )
+                obj = Reservation.objects.create(
+                    student_id=int(request.POST.get('student_id')),
+                    computer_id=int(request.POST.get('id_com')),
+                    start_date=request.POST.get('start_time'),
+                    end_date=request.POST.get('End_time'),
+                    date=str(request.POST.get('date_time'))
+                )
+                for l in Computer.objects.all():
+                    if (l.id == obj.computer.id):
+                        obj.computer.status = False
+                    obj.computer.save()
     context = {
+        'computer': Computer.objects.all(),
+        'loop': [1, 2, 3, 4, 5, 6, 7, 8],
+        'reservation': Reservation.objects.all()
     }
-    return render(request,'index.html')
+    return render(request, 'index.html', context)
